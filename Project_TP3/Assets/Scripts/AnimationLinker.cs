96fafa36a5317931e628d8803d41d2f1eb7a10e3
Pedro Pereira <pepereira@reseautique.osq>
2024-12-02 09:11:07 -0500

    Few changes at school

--- Project_TP3/Assets/Scripts/AnimationLinker.cs
+++ Project_TP3/Assets/Scripts/AnimationLinker.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationMaker : MonoBehaviour
+public class AnimationLinker : MonoBehaviour
 {
     public void ResetAttack()
     {
